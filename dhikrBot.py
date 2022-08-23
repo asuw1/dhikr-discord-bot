7b8d58dc65b248a4c4a14e537e84d9f21e6b4598
asuw1 <63924584+asuw1@users.noreply.github.com>
2022-08-23 12:40:44 +0300

    Create dhikrBot.py

---
+++ dhikrBot.py
@@ -0,0 +1,27 @@
+import discord
+import math
+import random
+import asyncio
+import csv
+
+dhikr = []
+
+with open('dhikr.csv', encoding="utf-8", newline='') as csvfile:
+    adhkar = csv.reader(csvfile, delimiter=',', quotechar='|')
+    dhikr = [i for i in adhkar]
+    dhikr = dhikr[0]
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print('Logged into discord as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        while True:
+            await message.channel.send(dhikr[math.floor(random.random()*(len(dhikr)+1))])
+            await asyncio.sleep(60)
+
+client = Client()
+client.run('API key')
